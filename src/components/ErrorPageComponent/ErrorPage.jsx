1d93ceb3d5b4ff9e7bc057022cf52b70b6b77f54
AtiqurRahman895 <emonhassan895@gmail.com>
2024-11-06 23:56:38 +0600

    ADDED TITLE FOR ERROR PAGE

--- src/components/ErrorPageComponent/ErrorPage.jsx
+++ src/components/ErrorPageComponent/ErrorPage.jsx
@@ -1,21 +1,29 @@
 // import PropTypes from 'prop-types';
 import { useRouteError } from 'react-router-dom';
 import errorImage from '../../assets/errorNotFoundImage.png'
+import { Helmet } from 'react-helmet-async';
 
 const ErrorPage = () => {
     const {error,status,statusText}= useRouteError();
     
     return (
-        <div className='h-lvh place-items-center grid content-center bg-white'>
-            {status==404?
-                <img src={errorImage} alt={`${status}, ${statusText}`} />:
-                <>
-                    <h1 className='font-extrabold text-9xl text-custom-purple'>!</h1>
-                    <h1 className='text-custom-purple'>{error.message||`${status}, ${statusText}`}</h1>
-                </>
+        <>
+            <Helmet>
+                <title>{statusText} | Gadget Heaven (PHeighthAssignment)</title>
+            </Helmet>
 
-            }
-        </div>
+            <div className='h-lvh place-items-center grid content-center bg-white'>
+                {status==404?
+                    <img src={errorImage} alt={`${status}, ${statusText}`} />:
+                    <>
+                        <h1 className='font-extrabold text-9xl text-custom-purple'>!</h1>
+                        <h1 className='text-custom-purple'>{error.message||`${status}, ${statusText}`}</h1>
+                    </>
+
+                }
+            </div>
+        </>
+
     );
 };
 
